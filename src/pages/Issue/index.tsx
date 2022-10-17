290f83625546e7ac65ef27e7979093e9a3a0d1f6
Isaac Aguiar <37755163+AguiarIsaac@users.noreply.github.com>
2022-10-17 20:07:46 +0000

    reactMarkdown

--- src/pages/Issue/index.tsx
+++ src/pages/Issue/index.tsx
@@ -1,5 +1,43 @@
+import { NavLink } from "react-router-dom";
+import ReactMarkdown from 'react-markdown';
+import { PostContent, PostInfo } from "./styles";
+import arrow from "../../assets/arrow.svg";
+import up from "../../assets/up.svg";
+import git from "../../assets/Type=github-brands.svg";
+import calendar from "../../assets/calendar.svg";
+import commit from "../../assets/commit.svg";
+import rehypeRaw from 'rehype-raw';
+
 export function Issue() {
     return (
-        <h1>ISSUE</h1>
+        <>
+            <PostInfo>
+                <div className="links">
+                    <NavLink to="/">
+                        <img src={arrow} id="arrow"/>
+                        Voltar
+                    </NavLink>
+
+                    <a href="#">
+                        Ver no GitHub
+                        <img src={up}/>
+                    </a>
+                </div>
+
+                <h4>JavaScript data types and data structures</h4>
+
+                <div className="moreInfos">
+                    <span><img src={git}/> <p>AguiarIsaac</p></span>
+                    <span><img src={calendar}/> <p>Miranda Computação</p></span>
+                    <span><img src={commit}/> <p>Comentários</p></span>
+                </div>
+            </PostInfo>
+
+            <PostContent>
+                <ReactMarkdown>
+                    # Hello World
+                </ReactMarkdown>
+            </PostContent>
+        </>
     )
 }
